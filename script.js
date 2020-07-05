fde956f0140db867319a062dcde8d480b70f3668
Tamir <kaliftamir@gmail.com>
2020-07-05 18:41:06 +0300

    I added validation function in js

--- script.js
+++ script.js
@@ -14,6 +14,39 @@ var renew = setInterval(function(){
 },500); 
 
 
-  
+function validation () {
 
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    var phone = document.getElementById("phone");
+    var email = document.getElementById("email");
+    
+    var mail = document.getElementById ("email");
+    mail.addEventListener("keyup", myValidation());
+    
+    function myValidation () {
+        if (mail.validity.typeMismatch) {
+           
+            mail.setCustomValidity("נא להקיש דואר אלקטרוני תקין")
+                
+        
+        } else {
+          mail.setCustomValidity("");
+          mail.style.color="green";
+         
+        }         
+    
+    };
 
+    
+    if (fname.checkValidity() & lname.checkValidity() & phone.checkValidity() & email.checkValidity()) {
+
+                  
+        alert("הטופס נשלח בהצלחה");   
+        
+    
+    } else {
+        alert("נא להשלים את השדות האדומים")
+    }    
+         
+}
